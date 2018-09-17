29f6692f726d1bc9b5d053ebb5ebde49b66a4e58
wintus <yuya0726+GitHub@gmail.com>
2018-09-18 03:27:06 +0900

    Refactor header by styled

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import Link from "next/link";
+import styled from "styled-components";
+
+const FlexBox = styled.div`
+  display: flex;
+`;
+
+const Item = styled.div`
+  margin: 0 ${1 / 4}rem;
+`;
 
 const Header = ({ pathname }: { pathname?: string }) => {
   const link = (path, name) => (
-    <Link href={path}>
-      <a
-        className={pathname === path ? "is-active" : ""}
-        style={{ margin: "0 .25rem" }}
-      >
-        {name}
-      </a>
-    </Link>
+    <Item>
+      <Link href={path}>
+        <a className={pathname === path ? "is-active" : ""}>{name}</a>
+      </Link>
+    </Item>
   );
 
   return (
-    <header style={{ display: "flex" }}>
-      {link("/", "Home")}
-      {link("/about", "About")}
+    <header>
+      <FlexBox>
+        {link("/", "Home")}
+        {link("/about", "About")}
+      </FlexBox>
     </header>
   );
 };
