ce2d68d0107671816a7414e65a23b807c7a157ae
wintus <yuya0726+GitHub@gmail.com>
2018-09-18 03:08:51 +0900

    Refactor link spacing in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import Link from "next/link";
 
 const Header = ({ pathname }: { pathname?: string }) => (
-  <header>
+  <header style={{ display: "flex" }}>
     <Link href="/">
-      <a className={pathname === "/" ? "is-active" : ""}>Home</a>
-    </Link>{" "}
+      <a
+        className={pathname === "/" ? "is-active" : ""}
+        style={{ margin: "0 .25rem" }}
+      >
+        Home
+      </a>
+    </Link>
     <Link href="/about">
-      <a className={pathname === "/about" ? "is-active" : ""}>About</a>
+      <a
+        className={pathname === "/about" ? "is-active" : ""}
+        style={{ margin: "0 .25rem" }}
+      >
+        About
+      </a>
     </Link>
   </header>
 );
